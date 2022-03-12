39ef834f55ec7a3ec01c4799e1da317031845702
yeboshuhuai <98003100+yeboshuhuai@users.noreply.github.com>
2022-03-12 15:21:01 +0800

    修改推进器输出

--- DRIVER_A/Driver_Propellors/Driver_Propellor.c
+++ DRIVER_A/Driver_Propellors/Driver_Propellor.c
@@ -23,7 +23,7 @@
 
 /* Private constants ---------------------------------------------------------*/
 #define DutyIDLE 1500
-
+#define MaxIDLE  500
 /* Private function prototypes -----------------------------------------------*/
 
 
@@ -35,14 +35,14 @@
   */
 void PROP_SpeedSet(PROP_DutyTypeDef* Speed) 
 {
-	__HAL_TIM_SetCompare(&HFL_TIM, HFL_Channel, DutyIDLE+Speed->HFL);
-	__HAL_TIM_SetCompare(&HFR_TIM, HFR_Channel, DutyIDLE+Speed->HFR);
-	__HAL_TIM_SetCompare(&HBL_TIM, HBL_Channel, DutyIDLE+Speed->HBL);
-	__HAL_TIM_SetCompare(&HBR_TIM, HBR_Channel, DutyIDLE+Speed->HBR);
-	__HAL_TIM_SetCompare(&VFL_TIM, VFL_Channel, DutyIDLE+Speed->VFL);
-	__HAL_TIM_SetCompare(&VFR_TIM, VFR_Channel, DutyIDLE+Speed->VFR);
-	__HAL_TIM_SetCompare(&VBL_TIM, VBL_Channel, DutyIDLE+Speed->VBL);
-	__HAL_TIM_SetCompare(&VBR_TIM, VBR_Channel, DutyIDLE+Speed->VBR);
+	__HAL_TIM_SetCompare(&HFL_TIM, HFL_Channel, DutyIDLE+Speed->HFL*MaxIDLE);
+	__HAL_TIM_SetCompare(&HFR_TIM, HFR_Channel, DutyIDLE+Speed->HFR*MaxIDLE);
+	__HAL_TIM_SetCompare(&HBL_TIM, HBL_Channel, DutyIDLE+Speed->HBL*MaxIDLE);
+	__HAL_TIM_SetCompare(&HBR_TIM, HBR_Channel, DutyIDLE+Speed->HBR*MaxIDLE);
+	__HAL_TIM_SetCompare(&VFL_TIM, VFL_Channel, DutyIDLE+Speed->VFL*MaxIDLE);
+	__HAL_TIM_SetCompare(&VFR_TIM, VFR_Channel, DutyIDLE+Speed->VFR*MaxIDLE);
+	__HAL_TIM_SetCompare(&VBL_TIM, VBL_Channel, DutyIDLE+Speed->VBL*MaxIDLE);
+	__HAL_TIM_SetCompare(&VBR_TIM, VBR_Channel, DutyIDLE+Speed->VBR*MaxIDLE);
 
 }
 
