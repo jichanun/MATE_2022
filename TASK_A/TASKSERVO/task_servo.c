e74efcf331b63ee8fa81945e0eef60df1f105151
cuizer <2480727939@qq.com>
2022-03-22 18:17:40 +0800

    舵机保护参数修改

--- TASK_A/TASKSERVO/task_servo.c
+++ TASK_A/TASKSERVO/task_servo.c
@@ -19,7 +19,7 @@ void ModeChooseandExcute(RemoteDataPortStruct	RemoteDataPort)
 				angle_DG.S_1=DTGL_Data.hand_x*11.11f;
 			  angle_DG.S_2=DTGL_Data.hand_y*11.11f;
 			  angle_DG.S_3=-1.33f*(DTGL_Data.finger1+DTGL_Data.finger2+DTGL_Data.finger3+DTGL_Data.finger4+DTGL_Data.finger5-250);
-				if((angle_DG.S_1>=550&&angle_DG.S_1<=2450)&&(angle_DG.S_1>=550&&angle_DG.S_1<=2450)&&(angle_DG.S_1>=550&&angle_DG.S_1<=2450))//舵机保护
+				 if((angle_DG.S_1>=-800&&angle_DG.S_1<=800)&&(angle_DG.S_1>=-800&&angle_DG.S_1<=800)&&(angle_DG.S_1>=-800&&angle_DG.S_1<=800))//舵机保护
 				SEVO_AngleSet(&angle_DG) ;
 		}
 		else if(Dummyswitch==1)//遥控器备用方案
@@ -27,7 +27,7 @@ void ModeChooseandExcute(RemoteDataPortStruct	RemoteDataPort)
 				angle_DG.S_1=(RemoteDataPort.Duoji_1-1024)*1.54f;
 			  angle_DG.S_2=(RemoteDataPort.Duoji_2-1024)*1.54f;
 			  angle_DG.S_3=(RemoteDataPort.Duoji_3-1024)*1.54f;
-			  if((angle_DG.S_1>=550&&angle_DG.S_1<=2450)&&(angle_DG.S_1>=550&&angle_DG.S_1<=2450)&&(angle_DG.S_1>=550&&angle_DG.S_1<=2450))//舵机保护
+			  if((angle_DG.S_1>=-800&&angle_DG.S_1<=800)&&(angle_DG.S_1>=-800&&angle_DG.S_1<=800)&&(angle_DG.S_1>=-800&&angle_DG.S_1<=800))//舵机保护
 				SEVO_AngleSet(&angle_DG) ;
 		}
 }
